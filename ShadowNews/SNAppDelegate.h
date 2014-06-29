19296d36edf9ca2dcb6c578b1f9f2c0d76cf9ba1
颜风 <1178328901@qq.com>
2014-06-29 22:53:35 +0800

    Initial Commit

---
+++ ShadowNews/SNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SNAppDelegate.h
+//  ShadowNews
+//
+//  Created by   颜风 on 14-6-29.
+//  Copyright (c) 2014年 ShadowNews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShadowNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShadowNews
+//
+//  Created by   颜风 on 14-6-29.
+//  Copyright (c) 2014年 ShadowNews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SNAppDelegate class]));
+    }
+}
