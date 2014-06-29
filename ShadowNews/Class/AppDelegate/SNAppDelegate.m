bd22e72d7f29b6b7687b7f04da8a43d7f8793371
颜风 <1178328901@qq.com>
2014-06-29 23:12:26 +0800

    初始化

--- ShadowNews/Class/AppDelegate/SNAppDelegate.m
+++ ShadowNews/Class/AppDelegate/SNAppDelegate.m
@@ -3,7 +3,7 @@
 //  ShadowNews
 //
 //  Created by   颜风 on 14-6-29.
-//  Copyright (c) 2014年 ShadowNews. All rights reserved.
+//  Copyright (c) 2014年 ShadowNews. All rights reserved.s
 //
 
 #import "SNAppDelegate.h"
